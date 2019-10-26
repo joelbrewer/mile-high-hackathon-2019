5315684b7fbc54a4c0deefcc94104b98c83ccb2a
Victor Montoya <victormontoya@Victors-MacBook-Pro.local>
2019-10-26 11:55:18 -0600

    added drawer

--- home-page/src/App.js
+++ home-page/src/App.js
@@ -9,6 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
 import FullWidthGrid from './components/grid.js'
+import TemporaryDrawer from './components/drawer.js'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       <AppBar position="static">
         <Toolbar variant="dense">
           <IconButton edge="start" className="" color="inherit" aria-label="menu">
-            <MenuIcon />
+            <TemporaryDrawer></TemporaryDrawer>
           </IconButton>
           <Typography variant="h6" color="inherit">
             Help Denver
