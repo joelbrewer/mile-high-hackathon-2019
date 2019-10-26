4a29487911dc3bbe72ef796454e6de620cfd8087
Spencer <spencer.brewerdigital.com>
2019-10-26 13:34:47 -0600

    cards dynamically load content from constants.js

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InfoCard from './InfoCard.jsx';
 
+import { faq_data } from '../constants'
+
 const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     justifyContent: 'center',
-    width: '100%'
+    alignItems: 'center',
+    width: '100%',
+    flexDirection: 'column',
   }
 }));
 
 export default function CardsList() {
   const classes = useStyles();
+  console.log(faq_data)
+
+  const infoCards = faq_data.map(data => (
+    <InfoCard
+      key={data.id}
+      question={data.question}
+      answer_html={data.answer_html}
+      image={data.image}
+    />
+    )
+  )
 
   return (
     <div className={classes.container}>
-      <InfoCard />
+        {infoCards}
     </div>
   );
 }
